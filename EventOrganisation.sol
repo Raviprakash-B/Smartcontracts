7eee352e8e1a01f8046621e0ebf48afeabf40774
Raviprakash <46168102+Raviprakash-B@users.noreply.github.com>
2023-07-04 15:36:23 +0530

    Update EventOrganisation.sol

--- EventOrganisation.sol
+++ EventOrganisation.sol
@@ -1,1 +1,12 @@
-#SPDX-License-Identifier:GPL-3.0
+//SPDX-License-Identifier:GPL-3.0
+pragma solidity >=0.5.0 <0.9.0;
+
+contract EventOrganisation{
+address organiser;
+string name;
+uint data;
+uint price;
+uint ticketCount;
+uint ticketRemaining;
+}
+
