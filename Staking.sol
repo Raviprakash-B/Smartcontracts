324f81ece56577910f9dc616df64c6c5ad00171c
Raviprakash <46168102+Raviprakash-B@users.noreply.github.com>
2023-07-11 21:48:36 +0530

    Create Staking.sol

---
+++ Staking.sol
@@ -0,0 +1,8 @@
+//Name : Staking Token
+//symbol: STT.
+//Decimal : 8
+//Total Supply : 500 million.
+//Create a smart contract that accept this staking token for generating rewards for the user , in each 10 blocks with the compound interest of 2%.
+//users can withdraw their deposit anytime and if not claimed it will be added to the principle for additional reward.
+//You are safe to assume any nuances of staking and reward and please comment it in relevant code block. You may deploy any EVM based block Chain in there testnet. 
+//Please send 10% of your total supply to this wallet address “0x4a78a8ac0c301D5f71Fbea7Bf797a2200403f28A”, so that we can test it in our side.
